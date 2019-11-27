3b801c535a8824f4bccaa96f18d9e5f1bd34e5ce
Jérémy DIARD <44467357+jeremd41@users.noreply.github.com>
2019-11-27 10:45:28 +0100

    Reparation beug Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, {Component} from 'react';
-import Router from "./router/Router";
+import React, { Component } from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import MenuBar from "./components/MenuBar";
+import Login from "./containers/Login";
+import Register from "./containers/Register";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Router>
-                    <MenuBar/>
-                </Router>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+        <Router forceRefresh={true}>
+          <MenuBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/connexion" component={Login} />
+            <Route path="/inscription" component={Register} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,29 +1,27 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // containers
 import Login from "../containers/Login";
 import Register from "../containers/Register";
 import Home from "../containers/Home";
-import Root from "../containers/Root";
 import NotFound from "../containers/NotFound"; // not found page for the user if he will use unknown routes
-import MenuBar from "../components/MenuBar"
+import MenuBar from "../components/MenuBar";
 
 class Direction extends Component {
-    render() {
-        return (
-            <Router>
-                <MenuBar />
-                <Switch>
-                    <Route exact path="/"  component={Root}/>
-                    <Route path="/connexion"  component={Login}/>
-                    <Route path="/accueil" component={Home}/>
-                    <Route path="/inscription" component={Register}/>
-                    <Route path="*" component={NotFound}/>
-                </Switch>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <MenuBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/connexion" component={Login} />
+          <Route path="/inscription" component={Register} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Direction;
